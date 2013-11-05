c4b640d623b6690d80a26128f6ff51a87eedb135
thomasgaillard <thomas.gaillard@me.com>
2013-11-05 13:47:50 +0100

    indeation agenda

--- src/gestion/app/App.java
+++ src/gestion/app/App.java
@@ -8,15 +8,15 @@ import gestion.agendas.Agenda;
 import gestion.agendas.Evenement;
 
 public class App {
-	
+
 	//main method
 	public static void main (String []args){
 		Evenement e1 = new Evenement();
-			e1.setNom("RDV Coiffeur");
-			e1.setLieu("Annecy");
-			e1.setDateHeure(new GregorianCalendar(2013,11,8,11,30));
+		e1.setNom("RDV Coiffeur");
+		e1.setLieu("Annecy");
+		e1.setDateHeure(new GregorianCalendar(2013,11,8,11,30));
 		Agenda ag1 = new Agenda("Perso");
-			ag1.ajouter(e1);
+		ag1.ajouter(e1);
 		System.out.println(e1.toString());
 
 	}
