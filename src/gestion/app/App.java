83756a7f79c3aa78df189efeda1b887ef3638a64
noxaw <maxence.mermoz@icloud.com>
2013-11-15 16:53:02 +0100

    last today

--- src/gestion/app/App.java
+++ src/gestion/app/App.java
@@ -3,9 +3,6 @@
 
 package gestion.app;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import gestion.agendas.Agenda;
